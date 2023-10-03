0a19fc56db000ed332c3a264df3d70fc6aff9647
alexkham <alexkha@netvision.net.il>
2023-10-03 06:29:38 +0300

    ask

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello to All My !!!!!???? Happy Big World 12345 67890"}
+
+@app.get("/ask")
+async def ask():
+    return {"message": "How are  you?"}
+
